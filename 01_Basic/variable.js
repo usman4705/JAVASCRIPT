766029b31904a515e2b78792a64ad44e497459c8
usman4705 <79322350+usman4705@users.noreply.github.com>
2024-04-17 13:57:49 +0000

    datatype

---
+++ 01_Basic/variable.js
@@ -0,0 +1,41 @@
+"Use strict"; // treat all code as a new version of JS
+const accountID=1233345;
+let accountEmail="kusman9000@gmail.com";
+var accountPassword="usman123";
+accountCity="Lahore";
+
+//accountID=123345;
+accountEmail="kUsman9000@gmail.com";
+accountPassword="usman123@";
+accountCity="karachi";
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
+
+
+//data-types
+//The first 3 are most important data-types
+
+//Numbers
+//Boolean
+//String
+
+//Null => standalone value
+//undefined
+//object
+//symbol //value that is unique and not useful now a days
+//BigInt //larger intergrs than  number type can hold
+
+
+
+
+
+console.log(typeof accountCity);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof 2);
+console.log(typeof 'usman');
+
+
+
+
+
